40185568b2c175cd7ecebfb9f3fc129d23722fc8
Nina Yujiri <ninayujiri@gmail.com>
2018-02-24 02:20:36 +0000

    Adds looping exercises

---
+++ which-school.js
@@ -0,0 +1,12 @@
+function whichSchool(age){
+
+if (age >= 13 && age <= 18){
+return "Secondary School";
+} 
+else if (age >= 19 && age <= 100){
+return "Lighthouse Labs";
+}
+else if (age >= 1 && age <= 12){
+return "Elementary School";
+}   
+}
